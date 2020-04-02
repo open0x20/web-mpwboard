6814770e30d339a6ade0a5cb4029d31c22a2c21d
Michael B <mbuelow42@protonmail.com>
2020-04-02 19:18:15 +0200

    no-ticket: added direct video link extraction to /apps/youtubedl

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,20 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    apis: {
+      apps: {
+        youtubedl: {
+          directLinks: []
+        }
+      }
+    }
+  },
+  mutations: {
+    setAppsYoutubedlDirectLinks: (state, links) => {
+      state.apis.apps.youtubedl.directLinks = links;
+    }
+  },
   actions: {},
   modules: {}
 });
--- src/views/apps/YouTubeDL.vue
+++ src/views/apps/YouTubeDL.vue
@@ -4,7 +4,7 @@
       <v-row align="center" justify="center">
         <v-col cols="12">
           <h2 class="title">YouTube-Downloader</h2>
-          <p class="font-weight-light">TODO</p>
+          <p class="font-weight-light">A tool to download, convert and manage YouTube&#8482; videos.</p>
         </v-col>
         <v-col cols="12">
           <Sheet title="Direct Link Extractor">
@@ -12,29 +12,60 @@
               <v-row>
                 <v-col cols="12">
                   <p class="font-weight-light">
-                    Paste a youtube link and click on extract!
+                    Paste a YouTube link and click on extract!
                   </p>
                   <v-row>
                     <v-col cols="8">
-                      <v-text-field solo label="Video-URL" hint="Example: https://youtube.com/watch?v=dQw4w9WgXcQ"></v-text-field>
+                      <v-text-field
+                        solo
+                        label="Video-URL"
+                        hint="Example: https://youtube.com/watch?v=dQw4w9WgXcQ"
+                        v-model="inputDirectLinks"
+                      ></v-text-field>
                     </v-col>
-                    <v-col cols="1">
-                      <v-btn class="" large >Extract</v-btn>
+                    <v-col cols="4">
+                      <v-btn class="" large @click="clickOnExtract"
+                        >Extract</v-btn
+                      >
                     </v-col>
                   </v-row>
                 </v-col>
                 <v-col cols="12">
-                  <v-data-table
-                          :headers="tableDirectLinks.tableHeaders"
-                          :items="tableDirectLinks.tableData"
-                          :sort-by="['itag']"
-                          :sort-desc="[false]"
-                          multi-sort
-                          hide-default-footer
-                          class="elevation-1"
-                  ></v-data-table>
+                  <template>
+                    <v-data-table
+                      :headers="tableDirectLinks.tableHeaders"
+                      :items="getTableDirectLinksData"
+                      :sort-by="['itag']"
+                      :sort-desc="[false]"
+                      hide-default-footer
+                      items-per-page="100"
+                      :loading="tableDirectLinks.isLoading"
+                      class="elevation-1"
+                    >
+                      <template v-slot:item.url="{ item }">
+                        <a :href="item.url" target="_blank">
+                          {{
+                            item.url.substr(0, 40) +
+                              "..." +
+                              item.url.substr(-10)
+                          }}</a
+                        >
+                      </template>
+                    </v-data-table>
+                  </template>
                 </v-col>
               </v-row>
+            </v-container>
+          </Sheet>
+        </v-col>
+        <v-col cols="12">
+          <Sheet title="Additional information">
+            <v-container>
+              <v-text-field
+                label="Direct Link URL"
+                value="http://localhost:3000/public/video_info.php"
+                disabled
+              ></v-text-field>
             </v-container>
           </Sheet>
         </v-col>
@@ -45,26 +76,56 @@
 
 <script>
 import Vue from "vue";
+import Axios from "axios";
 import Sheet from "@/components/Sheet.vue";
+import { mapState } from "vuex";
 
 export default Vue.extend({
   name: "YouTubeDL",
   components: { Sheet },
   data: () => ({
-    apiDirectLinksUrl: "http://localhost:3000/public/video_info.php?url=",
+    inputDirectLinks: "",
     tableDirectLinks: {
+      isLoading: false,
       tableHeaders: [
         {
-          text: "itag",
+          text: "iTag",
           align: "start",
-          sortable: false,
+          sortable: true,
           value: "itag"
         },
-        { text: "format", value: "format" },
-        { text: "url", value: "url" }
+        { text: "Format", value: "format" },
+        { text: "Url", value: "url" }
       ],
       tableData: []
+    }
+  }),
+  methods: {
+    onInputChangedDirectLinks: function(event) {
+      console.log(event.target);
+      this.inputDirectLinks = event.target.value;
     },
+    clickOnExtract: function() {
+      this.tableDirectLinks.isLoading = true;
+      Axios.get(
+        "http://localhost:3000/public/video_info.php" +
+          "?url=" +
+          encodeURI(this.inputDirectLinks)
+      )
+        .then(response => {
+          this.$store.state.apis.apps.youtubedl.directLinks =
+            response.data.links;
+          this.tableDirectLinks.isLoading = false;
+        })
+        .catch(error => {
+          this.$store.state.apis.apps.youtubedl.directLinks = [];
+          this.tableDirectLinks.isLoading = false;
+          console.log(error);
+        });
+    }
+  },
+  computed: mapState({
+    getTableDirectLinksData: state => state.apis.apps.youtubedl.directLinks
   })
 });
 </script>
