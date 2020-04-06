848754161a1d41dc36435fc4f1020e5846058691
Michael B <mbuelow42@protonmail.com>
2020-04-06 20:55:58 +0200

    no-ticket: fixed wrong https port form 80 to 443 (sorry)

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
 export default class Globals {
     public static API_URL__NAS_BROWSER = 'https://localhost:404/';
-    public static API_URL__YTDL_DIRECT_LINK_EXTRACTOR = 'https://pollux-server.de:80/ytdl/video_info.php';
+    public static API_URL__YTDL_DIRECT_LINK_EXTRACTOR = 'https://pollux-server.de:443/ytdl/video_info.php';
 }
