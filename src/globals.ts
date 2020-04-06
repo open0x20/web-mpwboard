e76f90eaf9d29b7d7f1230bac509b5132bd681c3
Michael B <mbuelow42@protonmail.com>
2020-04-06 18:06:15 +0200

    no-ticket: changed http to https for custom app backend urls

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
 export default class Globals {
-    public static API_URL__NAS_BROWSER = 'http://localhost:80/index.php';
-    public static API_URL__YTDL_DIRECT_LINK_EXTRACTOR = 'http://localhost:3000/public/video_info.php';
+    public static API_URL__NAS_BROWSER = 'https://localhost:404/';
+    public static API_URL__YTDL_DIRECT_LINK_EXTRACTOR = 'https://pollux-server.de:80/ytdl/video_info.php';
 }
