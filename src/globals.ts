bdbf2b444c341fc03493b3026d343c0eec5fbe65
Michael B <mbuelow42@protonmail.com>
2021-01-21 21:30:24 +0100

    NO-TICKET: removed/renamed some globals

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,4 @@
 export default class Globals {
     public static API_URL__NAS_BROWSER = '';
-    public static API_URL__YTDL_DIRECT_LINK_EXTRACTOR = '';
-    public static API_URL__YTDL_CONVERTER = '';
+    public static API_URL__ATAC = '';
 }
